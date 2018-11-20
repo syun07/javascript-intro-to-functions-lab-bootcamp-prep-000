69278b15c0a39ca8bf7d546aebecbab88d8351c3
Sarah Yun <sarahyun7@gmail.com>
2018-11-20 21:06:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
   return "I can't hear you!"
   }
-  if (string.toUpperCase() === string) {
+  if (string === string.toUpperCase()) {
     return "YES INDEED!"
   }
   if (string === "I love you, Grandma.") {
